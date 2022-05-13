d4f7bb0d4fb3a53a01782531156aaae36814dae1
Pablo Alonso <jackercito@gmail.com>
2022-05-13 14:12:34 +0200

    comit

--- src/app/vCardApp/services/v-card.service.ts
+++ src/app/vCardApp/services/v-card.service.ts
@@ -1,18 +1,18 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
-import { environment } from './../../../environments/environment'
 import { vCard } from './../models/vCard.model'
 
 @Injectable({
   providedIn: 'root'
 })
 export class VCardService {
+  API_URL: string = 'https://api.pacisa.es/api/vCard'
 
   constructor(private http: HttpClient) { }
 
   getVCard$(id: string): Observable<vCard> {
     return this.http
-      .get<vCard>(`${environment.API_URL}/vCard/${id}`)
+      .get<vCard>(`${this.API_URL}/vCard/${id}`)
   }
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,3 @@
 export const environment = {
   production: true,
-
-  API_URL: 'https://api.pacisa.es/api/vCard'
 };
